3396407078c2f8135279d9725959733ed7e052a3
Thomas Stewart <thomas.stewart0494@gmail.com>
2023-12-04 13:46:25 +0200

    Day 4

--- 2023/day-04/src/part1.rs
+++ 2023/day-04/src/part1.rs
@@ -1,5 +1,46 @@
-pub fn part_one(input: &str) -> bool {
-    true
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let matching_cards = get_matching_cards(line).len() as u32;
+
+            if matching_cards == 0 {
+                0
+            } else {
+                2_i32.pow(matching_cards - 1) as u32
+            }
+        })
+        .collect::<Vec<u32>>()
+        .into_iter()
+        .sum()
+}
+
+pub fn get_card_numbers(input: &str) -> Vec<u32> {
+    input
+        .split_whitespace()
+        .map(|num| num.parse::<u32>().unwrap())
+        .collect()
+}
+
+pub fn get_matching_cards(input: &str) -> Vec<u32> {
+    let (player_card, score_card) = input
+        .split_once(": ")
+        .unwrap()
+        .1
+        .splitn(2, "| ")
+        .collect_tuple()
+        .unwrap();
+
+    let player_numbers = get_card_numbers(player_card);
+    let score_numbers = get_card_numbers(score_card);
+
+    player_numbers
+        .iter()
+        .filter(|num| score_numbers.contains(num))
+        .cloned()
+        .collect()
 }
 
 #[cfg(test)]
@@ -10,18 +51,23 @@ mod tests {
     fn part_one_test() {
         struct TestCase {
             input: &'static str,
-            expected: bool,
+            expected: u32,
         }
 
         let test_cases = vec![
             TestCase {
-                input: "test",
-                expected: true,
+                input: r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#,
+                expected: 13,
             },
-            // TestCase {
-            //     input: "test",
-            //     expected: true,
-            // },
+            TestCase {
+                input: include_str!("../input/input1.txt"),
+                expected: 27059,
+            },
         ];
 
         for test_case in test_cases {
--- 2023/day-04/src/part2.rs
+++ 2023/day-04/src/part2.rs
@@ -1,5 +1,26 @@
-pub fn part_two(input: &str) -> bool {
-    true
+use std::collections::HashMap;
+
+use crate::part1::get_matching_cards;
+
+pub fn part_two(input: &str) -> u32 {
+    let mut card_counter: HashMap<u32, u32> = HashMap::new();
+
+    input
+        .lines()
+        .enumerate()
+        .map(|(card_no, line)| ((card_no + 1) as u32, get_matching_cards(line)))
+        .for_each(|(card_no, matching_cards)| {
+            card_counter.entry(card_no).or_insert(1);
+            let bonus_cards_count = matching_cards.len() as u32;
+            for n in (card_no + 1)..(card_no + bonus_cards_count + 1) {
+                card_counter.insert(
+                    n,
+                    card_counter.get(&n).unwrap_or(&1) + card_counter[&card_no],
+                );
+            }
+        });
+
+    card_counter.values().sum()
 }
 
 #[cfg(test)]
@@ -10,18 +31,23 @@ mod tests {
     fn part_two_test() {
         struct TestCase {
             input: &'static str,
-            expected: bool,
+            expected: u32,
         }
 
         let test_cases = vec![
             TestCase {
-                input: "test",
-                expected: true,
+                input: r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#,
+                expected: 30,
             },
-            // TestCase {
-            //     input: "test",
-            //     expected: true,
-            // },
+            TestCase {
+                input: include_str!("../input/input2.txt"),
+                expected: 5744979,
+            },
         ];
 
         for test_case in test_cases {
