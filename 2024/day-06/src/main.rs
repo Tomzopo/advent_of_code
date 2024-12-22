1b0d76a2a49fe46bd7f182991bb3900889dabc58
Thomas Stewart <thomas.stewart0494@gmail.com>
2024-12-22 13:27:39 +0200

    Day 6 progress

--- 2024/day-06/src/main.rs
+++ 2024/day-06/src/main.rs
@@ -15,6 +15,46 @@ fn part_two(input: &str) -> i64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn test_part_one_example() {}
+    fn test_part_one_example() {
+        let input = r#"....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."#;
+        assert_eq!(part_one(input), 41);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let input = include_str!("../input/input.txt");
+        assert_eq!(part_one(input), 0);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let input = r#"....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."#;
+        assert_eq!(part_two(input), 0);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = include_str!("../input/input.txt");
+        assert_eq!(part_two(input), 0);
+    }
 }
