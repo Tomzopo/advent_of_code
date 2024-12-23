00002f10496bce35bb37178655c34274d8ca2b43
Thomas Stewart <thomas.stewart0494@gmail.com>
2024-12-24 00:08:54 +0200

    Day 8 prep

---
+++ 2024/day-08/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let input = include_str!("../input/input.txt");
+    println!("{}", part_one(input));
+    println!("{}", part_two(input));
+}
+
+fn part_one(input: &str) -> i64 {
+    todo!()
+}
+
+fn part_two(input: &str) -> i64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_example() {
+        let input = r#"............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"#;
+        assert_eq!(part_one(input), 14);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let input = include_str!("../input/input.txt");
+        assert_eq!(part_one(input), 0);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let input = r#""#;
+        assert_eq!(part_two(input), 0);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = include_str!("../input/input.txt");
+        assert_eq!(part_two(input), 0);
+    }
+}
